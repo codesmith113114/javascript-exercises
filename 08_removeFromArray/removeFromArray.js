a33cbfd20e8e7f45bc31f8d856ee618bd14557fb
codesmith <amn2003ktr@gmail.com>
2025-05-12 20:09:20 +0530

    08 is solved

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...arg) {
+  // const index = arr.indexOf(arg);
+  // arr.splice(index, 1);
+  // return arr
+  const res = arr.filter((item) => {
+    return !arg.includes(item)
+  })
+  return res
 };
 
 // Do not edit below this line
