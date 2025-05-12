2300c0f960a30e4a4ef4e64a0c58262257f81083
codesmith <amn2003ktr@gmail.com>
2025-05-12 21:28:35 +0530

    11 solved

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (f) {
+  const c = (f - 32) / 9 * 5;
+  return Number(c.toFixed(1))
+
+
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (c) {
+  const f = ((c / 5) * 9) + 32;
+  return Number(f.toFixed(1))
 };
 
 // Do not edit below this line
