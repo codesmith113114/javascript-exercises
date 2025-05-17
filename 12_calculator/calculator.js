eb0cc660cc8d25f0770252692f1538543b6d0286
codesmith <amn2003ktr@gmail.com>
2025-05-17 16:29:20 +0530

    12_calculator solved

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function (...args) {
+  return args.reduce((previous, current) => {
+    return previous + current;
+  }, 0)
 };
 
-const subtract = function() {
-	
+const subtract = function (...args) {
+  return args.reduce((prev, curr) => {
+    return prev - curr;
+  })
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((previous, current) => {
+    return previous + current
+  }, 0)
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((prev, curr) => prev * curr)
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return Math.pow(a, b)
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  let producnt = 1;
+  for (let i = n; i > 0; i--) {
+    producnt *= i;
+  }
+  return producnt;
 };
 
 // Do not edit below this line
