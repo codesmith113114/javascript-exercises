0e59ef7cd2425d463d895991f8548c837b48efae
codesmith <amn2003ktr@gmail.com>
2025-05-18 14:32:20 +0530

    solved

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,4 +1,14 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  const cleanedStr = str.toLowerCase().split("").filter((item) => {
+    return alphanumerical.includes(item)
+  }).join("");
+
+  const reversedString = cleanedStr.split("").reverse().join("");
+
+  return reversedString === cleanedStr
+
+
 
 };
 
--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function (a) {
 
+  let n = parseInt(a)
+
+  if (n > 0) {
+
+    let arr = [1, 1]
+    for (let i = 2; i < n; i++) {
+      arr[i] = arr[i - 1] + arr[i - 2]
+    }
+    return arr[n - 1];
+  }
+  if (n < 0) {
+    return "OOPS";
+  }
+  else {
+    return 0;
+  }
 };
 
 // Do not edit below this line
