6bd313cfcb574a7d25a4c0878c255d2b04c31dbe
codesmith <amn2003ktr@gmail.com>
2025-05-18 14:31:53 +0530

    solved

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+  let output = arr.reduce((previous, current) => {
+    if (!previous.yearOfDeath) {
+      previous.yearOfDeath = new Date().getUTCFullYear();
+    }
 
+    if (!current.yearOfDeath) {
+      yearOfDeath = new Date().getUTCFullYear();
+    }
+
+    if ((previous.yearOfDeath - previous.yearOfBirth) < (current.yearOfDeath - current.yearOfBirth)) {
+      previous = current;
+    }
+    return previous;
+  })
+  return output;
 };
 
 // Do not edit below this line
